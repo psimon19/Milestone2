409f551f6c604e3102ad7111eb7e3966685e44f2
Peter Simon <psimon@gatech.edu>
2017-01-31 12:03:59 -0500

    Person5.java completed

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -30,8 +30,10 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+        //Person 5 put your implementation here
+        String a = input.substring(0, 1);
+        String b = input.substring(1, 2);
+        return input.substring(2) + a + b + "";
 	}
 	
 	/**
